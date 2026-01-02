05494389ed826b4cb3a59e0e06772a3f82f9f9a5
Nikita Hosalo <hosalmyk@cvut.cz>
2026-01-02 19:09:47 +0100

    fixing logic of gallery and registration

---
+++ second_page.js
@@ -0,0 +1,171 @@
+/* --- МОДАЛЬНОЕ ОКНО (ВХОД / РЕГИСТРАЦИЯ) --- */
+const modal = document.getElementById('auth-modal-id');
+const signInBtn = document.getElementById('reg_signin_id');
+const logInBtn = document.getElementById('reg_login_id');
+const closeBtn = document.getElementById('icon_cross_id');
+
+const modalTitle = document.getElementById('modal-title');
+const signInForm = document.getElementById('signin-form');
+const logInForm = document.getElementById('login-form');
+
+// Открытие Sign In из шапки
+signInBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+    logInForm.classList.add('hidden');
+    signInForm.classList.remove('hidden');
+    modalTitle.textContent = 'Sign in';
+});
+
+// Закрытие крестиком
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('open');
+});
+
+// Открытие Log In из шапки
+logInBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+    signInForm.classList.add('hidden');
+    logInForm.classList.remove('hidden');
+    modalTitle.textContent = 'Log in';
+});
+
+// Закрытие при клике вне окна
+window.addEventListener('click', (event) => {
+    if (event.target === modal){
+        modal.classList.remove('open');
+    }
+});
+
+/* --- МОДАЛЬНОЕ ОКНО (КНОПКИ В ФУТЕРЕ) --- */
+const signInEndBtn = document.getElementById('reg_signin_footer_id');
+const logInEndBtn = document.getElementById('reg_login_footer_id');
+
+signInEndBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+    logInForm.classList.add('hidden');
+    signInForm.classList.remove('hidden');
+    modalTitle.textContent = 'Sign in';    
+});
+
+logInEndBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+    signInForm.classList.add('hidden');
+    logInForm.classList.remove('hidden');
+    modalTitle.textContent = 'Log in';
+});
+
+
+/* --- СЛАЙДЕР (ГАЛЕРЕЯ) --- */
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+const tracks = document.querySelectorAll('.slider-track');
+
+const itemsVisible = 4;
+const originalTotalItems = tracks[0].children.length; 
+
+const itemWidth = 25; 
+
+tracks.forEach(track => {
+    for (let i = 0; i < itemsVisible + 3; i++) {
+        const clone = track.children[i].cloneNode(true);
+        track.appendChild(clone);
+    }
+
+    track.style.transition = 'transform 0.5s ease-in-out';
+});
+
+let currentSlideIndex = 0;
+let isAnimating = false;
+
+function updateSlider(withAnimation = true) {
+    const offset = -(currentSlideIndex * itemWidth);
+    
+    tracks.forEach(track => {
+        track.style.transition = withAnimation ? 'transform 0.5s ease-in-out' : 'none';
+        track.style.transform = `translateX(${offset}%)`;
+    });
+}
+
+btnNext.addEventListener('click', () => {
+    if (isAnimating) return; 
+    isAnimating = true;
+
+    currentSlideIndex++;
+    updateSlider(true);
+
+    if (currentSlideIndex === originalTotalItems) {
+        setTimeout(() => {
+            currentSlideIndex = 0; 
+            updateSlider(false);  
+            isAnimating = false;   
+        }, 500);
+    } else {
+        setTimeout(() => {
+            isAnimating = false;
+        }, 500);
+    }
+});
+
+btnPrev.addEventListener('click', () => {
+    if (isAnimating) return;
+    
+    if (currentSlideIndex > 0) {
+        isAnimating = true;
+        currentSlideIndex--;
+        updateSlider(true);
+        setTimeout(() => { isAnimating = false; }, 500);
+    } 
+    else {
+        isAnimating = true;
+        currentSlideIndex = originalTotalItems - 1;
+        updateSlider(true);
+        setTimeout(() => { isAnimating = false; }, 500);
+    }
+});
+
+/* --- СКРЫТЬ / ПОКАЗАТЬ ПАРОЛЬ --- */
+const passToggleBtn = document.querySelectorAll('.password-toggle');
+passToggleBtn.forEach((toggle) =>{
+    toggle.addEventListener('click', () => {
+        const passInputType = toggle.previousElementSibling
+        if (passInputType.type === 'password'){
+            passInputType.type = 'text';
+            toggle.textContent = '🙈';
+        }
+        else{
+            passInputType.type = 'password';
+            toggle.textContent = '👁';
+        }
+    });
+});
+
+/* --- ЗАГРУЗКА ФОТО --- */
+const fileInput = document.getElementById('user_photo_form');
+const photoConteiner = document.getElementById('user_photo_id');
+
+fileInput.addEventListener('change', function()  {
+    const file = this.files[0];
+    if (file){
+        const reader = new FileReader();
+        reader.onload = function(event){
+            const url = event.target.result;
+            photoConteiner.style.backgroundImage = `url(${url})`;
+            photoConteiner.classList.add('has-image');
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
+/* --- АНИМАЦИЯ ПОЯВЛЕНИЯ ПРИ СКРОЛЛЕ --- */
+const hiddenElements = document.querySelectorAll('.hidden-element');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
